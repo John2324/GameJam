649927e4f7e00191183a346603f48083647ae2bc
John <john@bytlabs.tech>
2020-10-19 06:25:21 -0700

    Create input map keys

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,34 +4,28 @@ const SPEED = 100
 const Gravity = 10
 const Jump_Power = -250
 const FLOOR = Vector2(0, -1)
-const fireball = preload ("res://Scenes/FireBall.tscn")
 
 var on_ground = false
-var velocity = Vector2()
+var velocity = Vector2(0,0)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		$AnimatedSprite.flip_h = false
 		velocity.x = SPEED
 		$AnimatedSprite.play('run')
-	elif Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.flip_h = true		
+	elif Input.is_action_pressed("left"):
+		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play('run')
 		velocity.x = -SPEED
-		
 	else:
-		velocity.x = 0
+		velocity.x = lerp(velocity.x, 0, 0.3)
 		$AnimatedSprite.play('idel')
 	
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("up"):
 		if on_ground == true:
 			velocity.y = Jump_Power
 			on_ground = false
 	
-	if Input.is_action_just_pressed("KEY_SPACE"):
-		var fireball = fireball.instance()
-		get_parent().add_child(fireball)
-		fireball.postion = $Position2D.global_position
 		
 	velocity.y += Gravity
 	if is_on_floor():
